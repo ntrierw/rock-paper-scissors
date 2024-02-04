64e41266625632aa80ce2dcd1b6158cc9f534e49
Nick Trierweiler <35035185+ntrierw@users.noreply.github.com>
2024-02-04 05:32:14 +0000

    finished building out logic

--- script.js
+++ script.js
@@ -13,7 +13,44 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-  
+    // if player is rock, beat scissors, lose paper
+    if (playerSelection == "rock") {
+        if (computerSelection == "scissors") {
+            return "You win! Rock beats scissors"
+        }
+        if (computerSelection == "paper") {
+            return "You lose! Paper beats rock"
+        }
+        if (computerSelection == "rock") {
+            return "It's a tie! Rock and rock"
+        }
+    }
+
+    // if player is paper, beat rock, lose scissors
+    if (playerSelection == "paper") {
+        if (computerSelection == "scissors") {
+            return "You lose! Scissors beats paper"
+        }
+        if (computerSelection == "paper") {
+            return "It's a tie! Paper and paper"
+        }
+        if (computerSelection == "rock") {
+            return "You win! Paper beats rock"
+        }
+    }
+
+    // if player is scissors, beat paper, lose rock
+    if (playerSelection == "scissors") {
+        if (computerSelection == "scissors") {
+            return "It's a tie! Scissors beats scissors"
+        }
+        if (computerSelection == "paper") {
+            return "You win! Scissors beats paper"
+        }
+        if (computerSelection == "rock") {
+            return "You lose! Rock beats scissors"
+        }
+    }
 }
 
 const playerSelection = "rock";
