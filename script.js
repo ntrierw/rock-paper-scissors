e1ee4127df9597be7d332e163e5f7ee69edcca54
Nick Trierweiler <35035185+ntrierw@users.noreply.github.com>
2024-02-04 05:10:08 +0000

    building get computer choice function

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+
+// get computer choice
+// returns a random selection of r, p, s
+function getComputerChoice() {
+    // init array of rock, paper, scissors
+    
+    // pick a random number between 0 and 2
+
+    // return the value of array at rand index
+}
+
 function playRound(playerSelection, computerSelection) {
   
 }
