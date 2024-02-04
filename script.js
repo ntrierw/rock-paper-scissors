3d84b605499cdbf9538ef98986a3a7cf5dc57b1e
Nick Trierweiler <35035185+ntrierw@users.noreply.github.com>
2024-02-04 05:15:11 +0000

    built out gcc function

--- script.js
+++ script.js
@@ -3,10 +3,13 @@
 // returns a random selection of r, p, s
 function getComputerChoice() {
     // init array of rock, paper, scissors
-    
+    const choice = ["rock", "paper", "scissors"];
+
     // pick a random number between 0 and 2
+    const index = Math.floor(Math.random() * 3);
 
     // return the value of array at rand index
+    return choice[index];
 }
 
 function playRound(playerSelection, computerSelection) {
